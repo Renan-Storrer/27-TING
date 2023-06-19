1ba7dc660e3ce5270f8fbda34cd9ea209a6f25b7
Renan-Storrer <storrerrenan@gmail.com>
2023-06-18 23:55:07 -0300

    feat: implementa código file_process(req03)

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,32 @@
+from .file_management import txt_importer
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    processed_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt_importer(path_file)),
+            "linhas_do_arquivo": txt_importer(path_file)
+        }
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return []
+
+    instance.enqueue(processed_data)
+    sys.stdout.write(str(processed_data))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        sys.stdout.write("Não há elementos\n")
+    else:
+        path_file = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        sys.stdout.write(str(file))
+    except IndexError:
+        sys.stderr.write(str("Posição inválida"))
